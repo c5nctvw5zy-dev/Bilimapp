8a250f4de19f720d04ac8fa02d45d389538b77e5
c5nctvw5zy-dev <c5nctvw5zy@privaterelay.appleid.com>
2026-01-13 04:00:48 +0500

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,19 @@
+const translations = {
+    kz: {
+        login:'Кіру',
+        password:'Құпия сөз',
+        logout:'Шығу'
+    },
+    ru: {
+        login:'Вход',
+        password:'Пароль',
+        logout:'Выйти'
+    },
+    en: {
+        login:'Login',
+        password:'Password',
+        logout:'Logout'
+    }
+};
+
+export default translations;
